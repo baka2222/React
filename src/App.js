7d4247363038dad53430050db351c8e006b5ae72
baka222 <ikaoss222@gmail.com>
2024-10-02 23:20:26 +0600

    hw1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+const Title = () => {
+  return <h2>Super informative title</h2>
+}
+
+const Header = () => {
+  return (
+      <header>
+        <Title/>
+        <p>This is a header</p>
+      </header>
+  )
+}
+
+const Content = () => {
+    return (
+        <div>
+            <Title/>
+            <p>This is a content</p>
+        </div>
+    )
+}
+
+const Footer = () => {
+    return (
+        <footer>
+            <Title/>
+            <p>This is a footer</p>
+        </footer>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <Header/>
+          <hr/>
+          <hr/>
+          <Content/>
+          <hr/>
+          <hr/>
+          <Footer/>
+      </>
   );
 }
 
